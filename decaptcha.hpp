7e671c855c724ed901c50ebd9c9b8e4add9cf2f0
kiki <kiki.caicai@gmail.com>
2013-07-08 00:01:06 +0800

    deCAPTCHA api change

--- decaptcha.hpp
+++ decaptcha.hpp
@@ -21,34 +21,64 @@
 
 namespace decaptcha{
 
+template<class ConstBuffer, class MsgSender, class Handler >
 class async_decaptcha_op{
+public:
 	async_decaptcha_op(boost::asio::io_service & io_service, 
 					const ConstBuffer & buf, MsgSender sender,
 					Handler handler)
+		:m_io_service(io_service), m_buffer(buf), m_sender(sender), m_handler(handler)
 	{
-		// 首先应该使用 
+		// TODO 使用机器识别算法
+		// TODO 使用人肉识别服务
+
+		// 让 XMPP/IRC 的聊友版面
+		
 	}
-	
+private:
+	boost::asio::io_service & m_io_service;
+	const ConstBuffer & m_buffer;
+	MsgSender m_sender;
+	Handler m_handler;	
 };
 
-/*
- * async_decaptcha 用于将 buf 表示的一个缓冲区(jpeg数据) 识别为一个文字,
- * 如果有可能的话, 还需要用到 sender 发送一些数据.
- * 识别完成后调用 handler 返回识别结果.
- * 
- * handler 的签名如下
- * 
- * void decaptcha_handler(boost::system::error_code ec, std::size_t id, std::string result)
- * {
- * 		
- * }
- */
-template<class ConstBuffer, class MsgSender, class Handler>
-void async_decaptcha(boost::asio::io_service & io_service, 
-					const ConstBuffer & buf, MsgSender sender,
-					Handler handler)
-{
-	async_decaptcha_op op(io_service,buf, sender, handler);
-}
+class deCAPTCHA{
+public:
+	deCAPTCHA(boost::asio::io_service & io_service)
+		:m_io_service(io_service)
+	{
+	}
+
+	/*
+	 * add_decoder 向系统添加验证码解码器.
+	 * 
+	 * 目前实现的解码器是 frientvc , 利用其他频道的聊友进行解码.
+	 */
+	add_decoder()
+	{
+	}
+
+	/*
+	* async_decaptcha 用于将 buf 表示的一个缓冲区(jpeg数据) 识别为一个文字,
+	* 如果有可能的话, 还需要用到 sender 发送一些数据.
+	* 识别完成后调用 handler 返回识别结果.
+	* 
+	* handler 的签名如下
+	* 
+	* void decaptcha_handler(boost::system::error_code ec, std::size_t id, std::string result)
+	* {
+	* 		
+	* }
+	*/
+	template<class ConstBuffer, class MsgSender, class Handler>
+	void async_decaptcha(const ConstBuffer & buf, MsgSender sender,
+						Handler handler)
+	{
+		async_decaptcha_op<ConstBuffer,MsgSender,Handler>
+							op(m_io_service,buf, sender, handler);
+	}
+private:
+	boost::asio::io_service & m_io_service;
+};
 
 }
