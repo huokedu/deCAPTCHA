7de30624381c521953791b597d32cf9fd3e1521b
microcai <microcaicai@gmail.com>
2013-08-25 03:07:09 +0800

    remove dep on console_out_str

--- include/deCAPTCHA/channel_friend_decoder.hpp
+++ include/deCAPTCHA/channel_friend_decoder.hpp
@@ -51,7 +51,7 @@ public:
 	#endif
 
 		// 同时向命令行也广播
-		std::cout << console_out_str("请查看qqlog目录下的vercode.jpeg 然后输入验证码: ") <<  std::flush ;
+		std::cout << "请查看qqlog目录下的vercode.jpeg 然后输入验证码: " <<  std::flush ;
 		std::cout.flush();
 
 		// 等待输入
@@ -154,5 +154,5 @@ channel_friend_decoder(boost::asio::io_service & io_service, Sender sender, Asyn
 	return channel_friend_decoder_t<Sender, AsyncInputer>(io_service, sender, async_inputer);
 }
 
-}
-}
+} // namespace decoder
+} // namespace decaptcha
