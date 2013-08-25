f6385bc859e365a01ba3a10179fc3e3a1abc4ea4
microcai <microcaicai@gmail.com>
2013-08-25 13:50:00 +0800

    console out use unicode string to ease the windows console

--- include/deCAPTCHA/channel_friend_decoder.hpp
+++ include/deCAPTCHA/channel_friend_decoder.hpp
@@ -51,8 +51,8 @@ public:
 	#endif
 
 		// 同时向命令行也广播
-		std::cout << "请查看qqlog目录下的vercode.jpeg 然后输入验证码: " <<  std::flush ;
-		std::cout.flush();
+		std::wcerr << L"请查看qqlog目录下的vercode.jpeg 然后输入验证码: " <<  std::flush ;
+		std::wcerr.flush();
 
 		// 等待输入
 
